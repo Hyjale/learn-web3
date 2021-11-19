baebad6d9e5a3ae4201b496cd263a80f4cc90843
Hyjale <saishidual@gmail.com>
2021-11-19 14:18:50 -0600

    Add token contract

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
 use std::str::FromStr;
 
-use web3::types::{H160, U256};
+use web3::types::{Address, H160, U256};
+use web3::contract::{Contract, Options};
 
 #[tokio::main]
 async fn main() -> web3::Result<()> {
@@ -17,7 +18,7 @@ async fn main() -> web3::Result<()> {
     accounts.push(address);
     println!("Accounts: {:?}", accounts);
 
-    /* Account balances in Wei (1 ETH = 1^18 Wei)*/
+    /* Account balances in Wei (1 ETH = 1^18 Wei) */
     for account in accounts {
         let balance = web3.eth().balance(account, None).await?;
         println!(
@@ -26,6 +27,20 @@ async fn main() -> web3::Result<()> {
             balance.checked_div(U256::exp10(18)).unwrap()
         );
     }
+
+    /* BAYC Contract */
+    let bayc_address = Address::from_str("0x4A11d5b67F53162066DdE2f85b048Bb3C9dbA4F0").unwrap();
+    let token_contract = Contract::from_json(web3.eth(), bayc_address, include_bytes!("erc20_abi.json")).unwrap();
+    let token_name: String = token_contract
+        .query("name", (), None, Options::default(), None)
+        .await
+        .unwrap();
+    let total_token_supply: U256 = token_contract
+        .query("totalSupply", (), None, Options::default(), None)
+        .await
+        .unwrap();
+
+    println!("Token name: {}, total supply: {}", token_name, total_token_supply);
 
     Ok(())
 }
